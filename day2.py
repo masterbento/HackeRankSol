760949777993f5dc06ecfd8fa19738108053da33
masterbento <bentolima100@gmail.com>
2018-04-25 09:58:50 -0100

    day 2

---
+++ day2.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+
+    total = round(meal_cost+ (meal_cost*(tip_percent/100) + (meal_cost*(tax_percent/100))))
+    #total = round(total)
+    print("The total meal cost is "+str(total)+" dollars.")
