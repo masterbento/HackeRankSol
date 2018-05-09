52579bdfb463c45201e3ed6376643ba3e40a1ded
masterbento <bentolima100@gmail.com>
2018-05-09 18:09:47 -0100

    Fixed the bug

--- minimaxsum.py
+++ minimaxsum.py
@@ -3,18 +3,18 @@
 import os
 import sys
 
-#
-# Complete the miniMaxSum function below.
-#
 def miniMaxSum(arr):
-    sortedarr = sorted(arr)
+    maximum = max(arr)
+    minimum = min(arr)
+    x = sum(arr)
 
-    maxsum= sum(arr)-arr[0]
-    minsum= sum(arr)-arr[-1]
+    #sortedarr = sorted(arr)
+    #maxsum= sum(arr)-arr[0]
+    #minsum= sum(arr)-arr[-1]
+    print(str(x-maximum),str(x-minimum))
 
-    print (sortedarr)
-    print(minsum,maxsum)
-    print (minsum<maxsum) #fix the bug
+    #print(minsum,maxsum)
+
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
